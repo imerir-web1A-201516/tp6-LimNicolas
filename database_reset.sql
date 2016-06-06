7196a983dbe4bd740fa46da0fb37f6a2958dc2cd
trololo <nicnl25@gmail.com>
2016-06-06 09:33:33 +0200

    Complété le schéma BDD

--- database_reset.sql
+++ database_reset.sql
@@ -1,3 +1,13 @@
 DROP TABLE IF EXISTS prets;
-CREATE TABLE prets (quoi varchar);
-INSERT INTO prets (quoi) VALUES ('test');
+
+CREATE TABLE prets (
+  pret_id SERIAL NOT NULL PRIMARY KEY,
+  pret_quoi VARCHAR(255) NOT NULL,
+  pret_qui VARCHAR(255) NOT NULL,
+  pret_etat INT NOT NULL
+);
+
+INSERT INTO prets (pret_quoi, pret_qui, pret_etat) VALUES ('Cable DVI', 'Jean-Patate', 0);
+INSERT INTO prets (pret_quoi, pret_qui, pret_etat) VALUES ('Cable HDMI', 'Jean-Moustache', 1);
+INSERT INTO prets (pret_quoi, pret_qui, pret_etat) VALUES ('Cable DisplayPort', 'Jean-Rémi', 0);
+INSERT INTO prets (pret_quoi, pret_qui, pret_etat) VALUES ('Cable VGA', 'Jean-Tomate', 2);
