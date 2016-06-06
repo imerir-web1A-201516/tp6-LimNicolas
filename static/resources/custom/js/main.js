f2a9f2e75387294b8f506c8000d35b6405afab23
trololo <nicnl25@gmail.com>
2016-06-06 09:40:44 +0200

    Liste des prets refreshée par ajax

--- static/resources/custom/js/main.js
+++ static/resources/custom/js/main.js
@@ -1,5 +1,24 @@
-$(function(){
+$(function() {
     
-    $('#title').text("À vous de jouer");
+    var dom_prets = $('#form_prets_list');
+    
+    function prets_refresh() {
+        $.ajax('/prets', {
+            success: function (prets) {
+                dom_prets.empty();
+
+                for (var pret_index in prets) {
+                    var pret = prets[pret_index];
+                    dom_prets.append($('<option>', {
+                        value: pret.pret_id,
+                        text: pret.pret_quoi + ' (' + pret.pret_qui + ')'
+                    }));
+                    console.log(pret)
+                }
+            }
+        });
+    }
+
+    prets_refresh();
 
 });
