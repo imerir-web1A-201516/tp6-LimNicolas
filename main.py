529baf784a4847bb6a70f60227222d759a46c69a
trololo <nicnl25@gmail.com>
2016-06-06 08:56:58 +0200

    Modification fichiers prof : indentation en multiple de 4 (voir PEP 8) + suppression commentaire encodage

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-# -*- coding: utf-8 -*-
 from flask import Flask, request, make_response
 import json, os, psycopg2, urlparse
-from db import Db # voyez db.py
+from db import Db  # voyez db.py
 
 app = Flask(__name__)
 app.debug = True
+
 
 ##################################################################
 
 @app.route('/debug/db/reset')
 def route_dbinit():
-  """Cette route sert à initialiser (ou nettoyer) la base de données."""
-  db = Db()
-  db.executeFile("database_reset.sql")
-  db.close()
-  return "Done."
+    """Cette route sert à initialiser (ou nettoyer) la base de données."""
+    db = Db()
+    db.executeFile("database_reset.sql")
+    db.close()
+    return "Done."
 
-#-----------------------------------------------------------------
+
+# -----------------------------------------------------------------
 
 @app.route('/prets', methods=['GET'])
 def prets_fetchall():
-  db = Db()
-  result = db.select("SELECT * FROM prets")
-  db.close()
-  
-  resp = make_response(json.dumps(result))
-  resp.mimetype = 'application/json'
-  return resp
+    db = Db()
+    result = db.select("SELECT * FROM prets")
+    db.close()
 
-#-----------------------------------------------------------------
+    resp = make_response(json.dumps(result))
+    resp.mimetype = 'application/json'
+    return resp
+
+
+# -----------------------------------------------------------------
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
